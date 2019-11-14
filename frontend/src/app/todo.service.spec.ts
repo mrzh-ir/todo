2db1202db8d5b6beb121138d33a9c4640ec52f3f
Bradford Hovinen <hovinen@gmail.com>
2019-11-14 10:58:01 +0100

    Switch from gRPC to REST

--- frontend/src/app/todo.service.spec.ts
+++ frontend/src/app/todo.service.spec.ts
@@ -1,9 +1,12 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { TodoService } from './todo.service';
 
 describe('TodoService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientModule],
+  }));
 
   it('should be created', () => {
     const service: TodoService = TestBed.get(TodoService);
--- frontend/src/app/todo.service.ts
+++ frontend/src/app/todo.service.ts
@@ -1,54 +1,25 @@
 import { Injectable } from '@angular/core';
-import { AddItemRequest, CompleteItemRequest, FetchListRequest, FetchListResponse } from './../proto/todo_service_pb';
-import { TodoServiceClient } from './../proto/Todo_serviceServiceClientPb';
-import * as grpcWeb from 'grpc-web';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
-  private client = new TodoServiceClient('http://localhost:8080', null, null);
-
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
 
   async fetchItems(): Promise<string[]> {
-    const request = new FetchListRequest();
-    return new Promise<string[]>((resolve, reject) => {
-      this.client.fetchList(request, {}, (err: grpcWeb.Error, response: FetchListResponse) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(response.getItemsList());
-        }
-      });  
-    });
+    return this.httpClient.get<FetchItemsResponse>('/api/items').toPromise().then((response) => response.items);
   }
 
   async addItem(item: string): Promise<void> {
-    const request = new AddItemRequest();
-    request.setLabel(item);
-    return new Promise<void>((resolve, reject) => {
-      this.client.addItem(request, {}, (err, _) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve();
-        }
-      });
-    });
+    return this.httpClient.post('/api/items', {'label': item}).toPromise().then(() => Promise.resolve());
   }
 
   async completeItem(item: string): Promise<void> {
-    const request = new CompleteItemRequest();
-    request.setLabel(item);
-    return new Promise<void>((resolve, reject) => {
-      this.client.completeItem(request, {}, (err, _) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve();
-        }
-      });
-    });
+    return this.httpClient.delete(`/api/items/${item}`).toPromise().then(() => Promise.resolve());
   }
+}
+
+class FetchItemsResponse {
+  items: string[];
 }
