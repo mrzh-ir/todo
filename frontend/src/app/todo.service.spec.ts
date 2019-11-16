1b1c80aa5063ed1db0e5d64928043fb8b50fc5bc
Bradford Hovinen <hovinen@gmail.com>
2019-11-16 13:22:02 +0100

    Add DTO and mapping to API layer

--- frontend/src/app/todo.service.spec.ts
+++ frontend/src/app/todo.service.spec.ts
@@ -1,15 +1,134 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import * as moment from 'moment';
 
+import { Type, Period, Item } from './item';
 import { TodoService } from './todo.service';
 
 describe('TodoService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [HttpClientModule],
-  }));
+  let subject: TodoService;
+  let httpMock: HttpTestingController;
 
-  it('should be created', () => {
-    const service: TodoService = TestBed.get(TodoService);
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    subject = TestBed.get(TodoService);
+    httpMock = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  const cases: Array<{dto: object, item: object}> = [
+    {
+      dto: {id: '100'},
+      item: {id: '100'},
+    },
+    {
+      dto: {label: 'An item'},
+      item: {label: 'An item'},
+    },
+    {
+      dto: {type: 'TASK'},
+      item: {type: Type.Task},
+    },
+    {
+      dto: {type: 'RECURRING'},
+      item: {type: Type.Recurring},
+    },
+    {
+      dto: {type: 'SHOPPING_ITEM'},
+      item: {type: Type.ShoppingItem},
+    },
+    {
+      dto: {amount: 100},
+      item: {amount: 100},
+    },
+    {
+      dto: {frequency: 2},
+      item: {frequency: 2},
+    },
+    {
+      dto: {period: 'DAY'},
+      item: {period: Period.Day},
+    },
+    {
+      dto: {period: 'WEEK'},
+      item: {period: Period.Week},
+    },
+    {
+      dto: {period: 'MONTH'},
+      item: {period: Period.Month},
+    },
+    {
+      dto: {period: 'YEAR'},
+      item: {period: Period.Year},
+    },
+    {
+      dto: {deadline: '2020-01-15'},
+      item: {deadline: moment('2020-01-15')},
+    },
+  ];
+
+  const toItemCases: Array<{dto: object, item: object}> = [
+    ...cases,
+    {
+      dto: {id: '100'},
+      item: {id: '100'},
+    },
+    {
+      dto: {type: 'invalid'},
+      item: {type: undefined},
+    },
+    {
+      dto: {period: 'invalid'},
+      item: {period: undefined},
+    },
+  ];
+
+  for (let {dto, item} of toItemCases) {
+    it(`should convert ${JSON.stringify(dto)} to ${JSON.stringify(item)} in fetching items`, async () => {
+      const resultPromise = subject.fetchItems();
+
+      const request = httpMock.expectOne('/api/items');
+      expect(request.request.method).toEqual('GET');
+      request.flush({items: [dto]});
+      expect(await resultPromise).toContain(jasmine.objectContaining(item));
+    });
+  }
+
+  for (let {dto, item} of cases) {
+    it(`should convert ${JSON.stringify(item)} to ${JSON.stringify(dto)} in adding an item`, async () => {
+      subject.addItem(item as Item);
+
+      const request = httpMock.expectOne('/api/items');
+      expect(request.request.method).toEqual('POST');
+      expect(request.request.body).toEqual(jasmine.objectContaining(dto));
+    });
+  }
+
+  it('should return list after adding item', async () => {
+    const resultPromise = subject.addItem(Item.emptyItem());
+
+    const request = httpMock.expectOne('/api/items');
+    request.flush({items: [{id: '100'}]});
+    expect(await resultPromise).toContain(jasmine.objectContaining({id: '100'}));
+  });
+
+  it('should complete an item', async () => {
+    subject.completeItem('100');
+
+    const request = httpMock.expectOne('/api/items/100');
+    expect(request.request.method).toEqual('DELETE');
+  });
+
+  it('should return list after completing item', async () => {
+    const resultPromise = subject.completeItem('100');
+
+    const request = httpMock.expectOne('/api/items/100');
+    request.flush({items: [{id: '200'}]});
+    expect(await resultPromise).toContain(jasmine.objectContaining({id: '200'}));
   });
 });
--- frontend/src/app/todo.service.ts
+++ frontend/src/app/todo.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import * as moment from 'moment';
 
-import { Item } from './item';
+import { Item, Type, Period } from './item';
 
 @Injectable({
   providedIn: 'root'
@@ -10,26 +11,118 @@ export class TodoService {
   constructor(private httpClient: HttpClient) {}
 
   async fetchItems(): Promise<Array<Item>> {
-    return this.httpClient.get<FetchItemsResponse>('/api/items').toPromise().then((response) => response.items);
+    return this.httpClient.get<FetchItemsResponse>('/api/items')
+        .toPromise()
+        .then((response) => response.items.map((itemDto) => ItemDto.toItem(itemDto)));
   }
 
   async addItem(item: Item): Promise<Array<Item>> {
-    return this.httpClient.post<AddItemResponse>('/api/items', item).toPromise().then(response => response.items);
+    return this.httpClient.post<AddItemResponse>('/api/items', ItemDto.fromItem(item))
+        .toPromise()
+        .then(response => response.items.map((itemDto) => ItemDto.toItem(itemDto)));
   }
 
   async completeItem(itemId: string): Promise<Array<Item>> {
-    return this.httpClient.delete<CompleteItemResponse>(`/api/items/${itemId}`).toPromise().then(response => response.items);
+    return this.httpClient.delete<CompleteItemResponse>(`/api/items/${itemId}`)
+        .toPromise()
+        .then(response => response.items.map((itemDto) => ItemDto.toItem(itemDto)));
   }
 }
 
 class FetchItemsResponse {
-  items: Array<Item>;
+  items: Array<ItemDto>;
 }
 
 class AddItemResponse {
-  items: Array<Item>;  
+  items: Array<ItemDto>;  
 }
 
 class CompleteItemResponse {
-  items: Array<Item>;
+  items: Array<ItemDto>;
+}
+
+class ItemDto {
+  id: string;
+  label: string;
+  type: string;
+  amount: number;
+  frequency: number;
+  period: string;
+  deadline: string;
+
+  static fromItem(item: Item): ItemDto {
+    const result = new ItemDto();
+    result.id = item.id;
+    result.label = item.label;
+    result.type = ItemDto.toTypeString(item.type);
+    result.amount = item.amount;
+    result.frequency = item.frequency;
+    result.period = ItemDto.toPeriodString(item.period);
+    result.deadline = item.deadline && item.deadline.format('YYYY-MM-DD');
+    return result;
+  }
+
+  private static toTypeString(type: Type): string {
+    switch (type) {
+      case Type.Task:
+        return 'TASK';
+      case Type.Recurring:
+        return 'RECURRING';
+      case Type.ShoppingItem:
+        return 'SHOPPING_ITEM';
+    }
+  }
+
+  private static toPeriodString(period: Period): string {
+    switch (period) {
+      case Period.Day:
+        return 'DAY';
+      case Period.Week:
+        return 'WEEK';
+      case Period.Month:
+        return 'MONTH';
+      case Period.Year:
+        return 'YEAR';
+    }
+  }
+
+  static toItem(dto: ItemDto): Item {
+    const result = Item.emptyItem();
+    result.id = dto.id;
+    result.label = dto.label;
+    result.type = ItemDto.toType(dto.type);
+    result.amount = dto.amount;
+    result.frequency = dto.frequency;
+    result.period = ItemDto.toPeriod(dto.period);
+    result.deadline = moment(dto.deadline);
+    return result;
+  }
+
+  private static toType(type: string): Type {
+    switch (type) {
+      case 'TASK':
+        return Type.Task;
+      case 'RECURRING':
+        return Type.Recurring;
+      case 'SHOPPING_ITEM':
+        return Type.ShoppingItem;
+      default:
+        return undefined;
+    }
+  }
+
+  private static toPeriod(period: string): Period {
+    switch (period) {
+      case 'DAY':
+        return Period.Day;
+      case 'WEEK':
+        return Period.Week;
+      case 'MONTH':
+        return Period.Month;
+      case 'YEAR':
+        return Period.Year;
+      default:
+        return undefined;
+    }
+  }
 }
