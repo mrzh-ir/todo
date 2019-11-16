5be90d6b1f464bc92333d4a6edc23ecc96aecb59
Bradford Hovinen <hovinen@gmail.com>
2019-11-16 15:13:32 +0100

    Support new API on backend

--- src/main/java/org/redischool/sd2/todo/api/TodoServiceController.java
+++ src/main/java/org/redischool/sd2/todo/api/TodoServiceController.java
@@ -6,34 +6,136 @@ import java.util.List;
 
 @RestController
 final class TodoServiceController {
-  private static final class FetchItemsResponseDto {
-    private final List<String> items;
+  private static final List<ItemDto> ITEMS =
+      List.of(ItemDto.withLabel("Butter"), ItemDto.withLabel("Sugar"), ItemDto.withLabel("Flour"));
 
-    FetchItemsResponseDto(List<String> items) {
+  @GetMapping("/api/items")
+  FetchItemsResponseDto fetchItems() {
+    return new FetchItemsResponseDto(ITEMS);
+  }
+
+  @PostMapping("/api/items")
+  AddItemResponseDto addItem(AddItemDto addItemDto) {
+    return new AddItemResponseDto(ITEMS);
+  }
+
+  @DeleteMapping("/api/items/{id}")
+  DeleteItemResponseDto deleteItem(@PathVariable("id") String id) {
+    return new DeleteItemResponseDto(ITEMS);
+  }
+
+  private static final class FetchItemsResponseDto {
+    private final List<ItemDto> items;
+
+    FetchItemsResponseDto(List<ItemDto> items) {
       this.items = List.copyOf(items);
     }
 
-    public List<String> getItems() {
+    public List<ItemDto> getItems() {
       return items;
     }
   }
 
-  private static final class AddItemDto {
-    private String item;
+  private static final class AddItemResponseDto {
+    private final List<ItemDto> items;
 
-    public void setItem(String item) {
-      this.item = item;
+    AddItemResponseDto(List<ItemDto> items) {
+      this.items = List.copyOf(items);
+    }
+
+    public List<ItemDto> getItems() {
+      return items;
     }
   }
 
-  @GetMapping("/api/items")
-  FetchItemsResponseDto fetchItems() {
-    return new FetchItemsResponseDto(List.of("Butter", "Sugar", "Flour"));
+  private static final class DeleteItemResponseDto {
+    private final List<ItemDto> items;
+
+    DeleteItemResponseDto(List<ItemDto> items) {
+      this.items = List.copyOf(items);
+    }
+
+    public List<ItemDto> getItems() {
+      return items;
+    }
   }
 
-  @PostMapping("/api/items")
-  void addItem(AddItemDto addItemDto) {}
+  private static final class ItemDto {
+    private static long nextId = 0;
+    String id;
+    String label;
+    String type;
+    Integer amount;
+    Integer frequency;
+    String period;
+    String deadline;
 
-  @DeleteMapping("/api/items/{item}")
-  void deleteItem(@PathVariable("item") String item) {}
+    static ItemDto withLabel(String label) {
+      ItemDto itemDto = new ItemDto();
+      itemDto.label = label;
+      itemDto.id = String.valueOf(nextId++);
+      return itemDto;
+    }
+
+    public String getId() {
+      return id;
+    }
+
+    public String getLabel() {
+      return label;
+    }
+
+    public String getType() {
+      return type;
+    }
+
+    public Integer getAmount() {
+      return amount;
+    }
+
+    public Integer getFrequency() {
+      return frequency;
+    }
+
+    public String getPeriod() {
+      return period;
+    }
+
+    public String getDeadline() {
+      return deadline;
+    }
+  }
+
+  private static final class AddItemDto {
+    String label;
+    String type;
+    Integer amount;
+    Integer frequency;
+    String period;
+    String deadline;
+
+    public void setLabel(String label) {
+      this.label = label;
+    }
+
+    public void setType(String type) {
+      this.type = type;
+    }
+
+    public void setAmount(Integer amount) {
+      this.amount = amount;
+    }
+
+    public void setFrequency(Integer frequency) {
+      this.frequency = frequency;
+    }
+
+    public void setPeriod(String period) {
+      this.period = period;
+    }
+
+    public void setDeadline(String deadline) {
+      this.deadline = deadline;
+    }
+  }
 }
