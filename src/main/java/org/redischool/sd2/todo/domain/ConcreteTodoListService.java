bfb9156d5f445f4e349a89651fb1b34ea94bb0ff
Bradford Hovinen <hovinen@gmail.com>
2019-11-18 15:11:17 +0100

    Throw UnsupportedOperationException

--- src/main/java/org/redischool/sd2/todo/domain/ConcreteTodoListService.java
+++ src/main/java/org/redischool/sd2/todo/domain/ConcreteTodoListService.java
@@ -9,26 +9,26 @@ import java.time.Period;
 final class ConcreteTodoListService implements TodoListService {
   @Override
   public void addTask(String label) {
-
+    throw new UnsupportedOperationException("Not implemented yet");
   }
 
   @Override
   public void addTaskWithDeadline(String label, LocalDate deadline) {
-
+    throw new UnsupportedOperationException("Not implemented yet");
   }
 
   @Override
   public void addRecurringTask(String label, Period recurrencePeriod) {
-
+    throw new UnsupportedOperationException("Not implemented yet");
   }
 
   @Override
   public void addShoppingItem(String label, int amount) {
-
+    throw new UnsupportedOperationException("Not implemented yet");
   }
 
   @Override
   public void markCompleted(String itemId) {
-
+    throw new UnsupportedOperationException("Not implemented yet");
   }
 }
