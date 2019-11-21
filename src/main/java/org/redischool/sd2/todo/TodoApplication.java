6fc959f4bcea908497eb12ee05ce26974b96b2ac
Bradford Hovinen <hovinen@gmail.com>
2019-11-21 19:08:30 +0100

    Configure injection of Clock

--- src/main/java/org/redischool/sd2/todo/TodoApplication.java
+++ src/main/java/org/redischool/sd2/todo/TodoApplication.java
@@ -2,10 +2,22 @@ package org.redischool.sd2.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Clock;
 
 @SpringBootApplication
 public class TodoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
+	}
+
+	@Configuration
+	static class ApplicationConfiguration {
+		@Bean
+		Clock provideClock() {
+			return Clock.systemDefaultZone();
+		}
 	}
 }
