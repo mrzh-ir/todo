4543cb0e68f9e5f120a2c3b98bffeaa3d8d76d86
Bradford Hovinen <hovinen@gmail.com>
2020-06-06 15:08:30 +0200

    Switch to kts and upgrade to latest npm dependencies

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+	java
+	id("org.springframework.boot") version "2.3.0.RELEASE"
+}
+
+group = "org.redischool.sd2"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation(platform("org.springframework.boot:spring-boot-dependencies:2.3.0.RELEASE"))
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation(project(":frontend"))
+
+	testImplementation("org.junit.jupiter:junit-jupiter:5.6.0")
+	testImplementation("org.assertj:assertj-core:3.14.0")
+	testImplementation("org.mockito:mockito-core:3.1.0")
+	testImplementation("org.mockito:mockito-junit-jupiter:3.1.0")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_11
+	targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks {
+	test {
+		useJUnitPlatform()
+	}
+}
---
+++ frontend/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    java
+    id("com.github.node-gradle.node") version "2.2.4"
+}
+
+node {
+    version = "12.18.0"
+    npmVersion = "6.13.0"
+    download = true
+}
+
+tasks.getByName<Jar>("jar") {
+    from("dist/todo-frontend")
+    into("static")
+    dependsOn(tasks.findByName("npm_run_build"))
+}
